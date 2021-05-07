60b2d0200163b87618777422f0d7d8a1006a15b4
Mathew George <mattugt@gmail.com>
2021-05-07 19:18:58 +0400

    Changed index.py

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "This is my first program on docker container!"
+    return "This is the second time running a propgram on docker container that too from Jenkins! Awesome.."
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
