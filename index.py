046da4e8b34f9feb8aa90c56fa8408edf204034f
Mathew George <mattugt@gmail.com>
2021-05-07 21:56:10 +0400

    final

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "This is the second time running a propgram on docker container that too from Jenkins! Awesome.."
+    return "This is the final time running this program on docker container with the image being pushed to dockerhub! Awesome.."
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
